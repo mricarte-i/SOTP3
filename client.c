8ef3d6d6afda43b43313ef2495405b77f2eab6a6
isampedro <isampedro@itba.edu.ar>
2020-06-19 18:32:15 -0300

    Last riddle almost there

--- client.c
+++ client.c
@@ -71,6 +71,10 @@ int main(int argc, char **argv){
     send(sockfd , chin_chu_lan_cha , strlen(chin_chu_lan_cha) , 0 );
     printf("10\n");
 
+    char* gdbme = "0x401dcf\n";
+    send(sockfd , gdbme , strlen(gdbme) , 0 );
+    printf("11\n");
+
     //FILE* quine = fopen()
     return 0;
 }
